0a1d99da50a809f2da5270949d11dabde779988e
potato0331 <mathcdy@naver.com>
2024-11-10 06:35:07 +0000

    ㅇㅇ

--- public/game/app.js
+++ public/game/app.js
@@ -3,7 +3,7 @@ const context = canvas.getContext("2d");
 const moveSound = new Audio("./resources/TAK.wav"); //음질 개구림 대체할거 어디서찾니..
 const errorSound = new Audio("./resources/error.mp3");
 const tostmessage = document.getElementById("wrongmove_tost");
-const tostmessage2 = document.getElementById("notthisturn_tost");
+const tostmessage_wrongturn = document.getElementById("notthisturn_tost");
 const tostmessage_playercolor = document.getElementById("playercolor_tost");
 
 var socket = io(); 
@@ -12,9 +12,7 @@ var area4game;
 socket.on("connected",(gameInfo) =>{
     tostmessage_playercolor.innerHTML="당신은 "+gameInfo.color+" 입니다";
     tostmessage_playercolor.classList.add("active");
-    setTimeout(() => {
-    tostmessage_playercolor.classList.remove("active");
-}, 1500);
+    setTimeout(() => { tostmessage_playercolor.classList.remove("active");}, 1500);
   area4game = new area4();
   area4game.playerColor = gameInfo.color;
   area4game.mainBoard = gameInfo.history;
@@ -28,6 +26,20 @@ socket.on("moveReceived",(moveHistory) =>{
 
 canvas.addEventListener("click", (e) => {
   let { inputX, inputY } = area4game.getPosition(e.offsetX, e.offsetY);
+  if (inputX < 0 || inputX > 8 || inputY < 0 || inputY > 8) {
+    return;
+  } //보드판 밖 클릭 시 무시
+  if (area4game.playerColor == "spectater"){
+    return;
+  } //관전자 일시 무시
+  if ((area4game.mainBoard.length != 0 && area4game.mainBoard[area4game.mainBoard.length - 1].color == area4game.playerColor) ||
+     (area4game.mainBoard.length == 0 && area4game.playerColor == "white")){
+    tostmessage_wrongturn.classList.add("active");
+    setTimeout(() => {
+    tostmessage_wrongturn.classList.remove("active");}, 500);
+    return;
+  } // 만일 이전 턴의 색이 나의 색과 같거나(연속으로 착수를 두번하거나), 첫 턴이고 백인데 착수하려 하면 토스트메세지를 띄우고 무시
+
   let MoveInfo = area4game.putStone(inputX,inputY);
   socket.emit("playerMove" , MoveInfo);
 });
--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+app.use(express.static("public"));
+
 let playerColor;
+var boardArray = Array.from(Array(9), () => new Array(9).fill(" "));
 var moveHistory = new Array();
 var playerList = {
   black : 0, 
@@ -12,7 +15,10 @@ var playerList = {
   spectater : new Array()
 };
 
-app.use(express.static("public"));
+function gameReset(){
+  boardArray = Array.from(Array(9), () => new Array(9).fill(" "));
+  moveHistory = new Array();
+} // boardArray와 moveHistory 초기화
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -22,22 +28,21 @@ io.on("connection", (socket) => {
 
   if (playerList.black == 0){
     playerList.black = socket.id;
-    console.log("a user connected, BLACK ,user id = " + socket.id);
     playerColor = "black";
   } 
   else if (playerList.white == 0){
     playerList.white = socket.id;
-    console.log("a user connected, WHITE ,user id = " + socket.id);
     playerColor = "white";
   } 
   else {
     playerList.spectater.push(socket.id)
-    console.log("a user connected, SPECTATER ,user id = " + socket.id);
     playerColor = "spectater"
   } // 빈칸에 플레이어 추가, 다만 빈칸이 없다면 spectater배열에 id추가
 
   socket.emit("connected" , {color : playerColor, history : moveHistory});
     //접속시 connected이벤트로 플레이어의 색상과 현재 수 현황 넘김
+  
+  console.log("a user connected, "+playerColor+", user id = " + socket.id);
   console.log(playerList);
 
   socket.on("disconnect", () => {
@@ -54,17 +59,21 @@ io.on("connection", (socket) => {
     } // 플레이어 id찾아서 제거, 없으면 spectater에서 찾아서 제거
 
     console.log(playerList);
+
+    if (playerList.black == 0 && playerList.white == 0){
+      gameReset()
+      console.log("resetting game...")
+      io.emit("moveReceived",moveHistory);
+    } // 플레이어가 아무도 없을 시 리셋
   });
 
   socket.on("playerMove" , (MoveInfo) =>{
     moveHistory.push(MoveInfo);
     io.emit("moveReceived",moveHistory);
   });
-  //moveinfo를 movehistory 에저장하고, movehistory를 모든 플레이어에게 전송
+  //moveinfo를 movehistory에 저장하고, movehistory를 모든 플레이어에게 전송
 
 });
-
-
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
